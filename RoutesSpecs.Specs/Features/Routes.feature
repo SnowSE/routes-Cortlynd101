6367c7c418145c02354d457566a34fd19a3d1118
Cortlynd101 <CortlyndCox@gmail.com>
2022-04-18 16:18:38 -0600

    Finished fleshing out the feature file.

--- RoutesSpecs.Specs/Features/Routes.feature
+++ RoutesSpecs.Specs/Features/Routes.feature
@@ -3,9 +3,9 @@
 
 @mytag
 Scenario Outline: Creating Possible Routes
-	Given the text file 
+	Given the text file C:\Users\Cortl\source\repos\routes-Cortlynd101\Routes\TestInput.txt
 	When creating possible routes
-	When looking at a certain route <number>
+	When looking at a certain route number <number>
 	Then the first city should be <firstCity>
 	Then the second city should be <secondCity>
 	Then the distance should be <distance>
@@ -15,4 +15,20 @@ Scenario Outline: Creating Possible Routes
 	| 1      | London    | Belfast    | 518      |
 	| 2      | Dublin    | Belfast    | 141      |
 
-Scenario Outline: 
+Scenario: Check Best Route Small Sample
+	Given the text file C:\Users\Cortl\source\repos\routes-Cortlynd101\Routes\TestInput.txt
+	When creating possible routes
+	And finding the best routes
+	Then the first city should be London
+	Then the second city should be Dublin
+	Then the third city should be Belfast
+	Then the distance should be 605
+
+Scenario: Check Best Route Large Sample
+	Given the text file C:\Users\Cortl\source\repos\routes-Cortlynd101\Routes\TestInput.txt
+	When creating possible routes
+	And finding the best routes
+	Then the first city should be toBeDetermined
+	Then the second city should be toBeDetermined
+	Then the third city should be toBeDetermined
+	Then the distance should be toBeDetermined
--- RoutesSpecs.Specs/StepDefinitions/RoutesStepDefinitions.cs
+++ RoutesSpecs.Specs/StepDefinitions/RoutesStepDefinitions.cs
@@ -8,6 +8,52 @@ namespace RoutesSpecs.Specs.StepDefinitions
         {
             this.context = context;
         }
+        [Given(@"the text file (.*)")]
+        public void GivenTheTextFile(string file)
+        {
+            context.Add("file", file);
+        }
 
+        [When(@"creating possible routes")]
+        public void WhenCreatingPossibleRoutes()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"looking at a certain route number (.*)")]
+        public void WhenLookingAtACertainRoute(int p0)
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"the first city should be (.*)")]
+        public void ThenTheFirstCityShouldBe(string firstCity)
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"the second city should be (.*)")]
+        public void ThenTheSecondCityShouldBe(string secondCity)
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"the distance should be (.*)")]
+        public void ThenTheDistanceShouldBe(int p0)
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"finding the best routes")]
+        public void WhenFindingTheBestRoutes()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"the third city should be (.*)")]
+        public void ThenTheThirdCityShouldBe(string thirdCity)
+        {
+            throw new PendingStepException();
+        }
     }
 }
